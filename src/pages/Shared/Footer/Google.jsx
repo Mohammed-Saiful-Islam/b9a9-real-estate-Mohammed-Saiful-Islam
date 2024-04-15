e5848416ec0c5a588a56d062d6d62aa1c9075abb
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-04-16 01:04:17 +0600

    social media sites links added

---
+++ src/pages/Shared/Footer/Google.jsx
@@ -0,0 +1,11 @@
+import { ExternalLink } from 'react-external-link';
+
+const MyComponent = () => (
+  <div>
+    <ExternalLink href="https://example.com">
+      <span>Visit the site</span>
+    </ExternalLink>
+  </div>
+);
+
+export default MyComponent;
