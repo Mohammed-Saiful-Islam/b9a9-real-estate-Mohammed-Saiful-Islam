7629b954730c0be7ef6cf1d638dfa2167e91a73a
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-04-15 22:07:44 +0600

    footer color changed

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -2,9 +2,10 @@ import { useParams } from "react-router-dom";
 import Header from "../Shared/Header/Header";
 import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
+import Footer from "../Shared/Footer/Footer";
 
 const News = () => {
-    const { id } = useParams();
+    const {id} = useParams();
     return (
         <div>
             <Header></Header>
@@ -13,11 +14,12 @@ const News = () => {
                 <div className="col-span-3">
                     <h2 className="text-5xl">News Detail</h2>
                     <p>{id}</p>
-                </div>
+                    </div>
                 <div>
                     <RightSideNav></RightSideNav>
                 </div>
             </div>
+            <Footer></Footer>
         </div>
     );
 };
--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,6 +1,6 @@
 
 import qZone1 from '../../../assets/qZone1.png';
-import qZone2 from '../../../assets/qZone2.png';
+// import qZone2 from '../../../assets/qZone2.png';
 import qZone3 from '../../../assets/qZone3.png';
 
 
@@ -16,7 +16,7 @@ const RightSideNav = () => {
                    
                 </button>
             </div>
-            <div className='p-4 mb-6'>
+            {/* <div className='p-4 mb-6'>
                 <h2 className="text-3xl mb-4">Find Us on</h2>
                 <a className='p-4 flex text-lg items-center border rounded-t-lg' href="">
                    
@@ -27,12 +27,12 @@ const RightSideNav = () => {
                 <a className='p-4 flex text-lg items-center border rounded-b-lg' href="">
                     
                 </a>
-            </div>
+            </div> */}
             {/* q zone */}
             <div className='p-4 space-y-3 mb-6'>
                 <h2 className="text-3xl">Q Zone</h2>
                 <img src={qZone1} alt="" />
-                <img src={qZone2} alt="" />
+                {/* <img src={qZone2} alt="" /> */}
                 <img src={qZone3} alt="" />
             </div>
         </div>
