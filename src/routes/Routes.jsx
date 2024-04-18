abffd7204686f259f2e57ec0cb059d6d8d9c1166
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-04-18 15:39:27 +0600

    errorPage added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,11 +5,13 @@ import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
 import News from "../pages/News/News";
 import PrivateRoute from "./PrivateRoute";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Root></Root>,
+        errorElement:<ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/',
